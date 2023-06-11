569d441a73f71bbaffbcf0c671b518685dfb3ad5
9beat <126194877+9beat@users.noreply.github.com>
2023-06-11 02:36:29 +0200

    img grid optimization

--- epibooks/src/components/LatestRealease.jsx
+++ epibooks/src/components/LatestRealease.jsx
@@ -16,11 +16,11 @@ export default function LatestRelease() {
         <Row>
             {
             horrorBooks.map( (book, i) => (
-            <Col key={i} xxxl={1} xl={2} lg={3} md={4} sm={6} xs={6} xxs={12} className="my-2 ">
-            <Card.Body className="body-card">
+            <Col key={i} xxxl={1} xl={2} lg={3} md={4} sm={6} xs={6} xxs={12} className="m-0">
+            <Card.Body className="body-card p-0 mt-6">
                 <Card.Img  key={i} src={book.img} alt={book.title} className="book-image"/>
-                    <Card.Title key={i}>{book.title}</Card.Title>
-                </Card.Body>
+                <Card.Title key={i} className="book-title mt-0 pt-0">{book.title}</Card.Title>
+            </Card.Body>
             </Col>
             ))
             }
